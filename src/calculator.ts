5d9dfa43e399d7f2cd8218eca35023077b697608
ramvignesh-b <ramvignesh@incubyte.co>
2025-02-05 17:16:59 +0530

    feat: handles newline as a separator

--- src/calculator.ts
+++ src/calculator.ts
@@ -2,7 +2,7 @@ export class Calculator {
     add(input: string): number {
         if (input === "")
             return 0;
-        const numbers = input.split(",");
+        const numbers = input.replace("\n", ",").split(",");
         return numbers.reduce((sum, number) => sum + parseInt(number), 0);
     }
 }
