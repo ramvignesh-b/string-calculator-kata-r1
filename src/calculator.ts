e3039fcfc23ad74ebff085eea0baa5529fda2f55
ramvignesh-b <ramvignesh@incubyte.co>
2025-02-05 17:14:49 +0530

    refactor: return the sm of the numbers for non empty string

--- src/calculator.ts
+++ src/calculator.ts
@@ -2,12 +2,7 @@ export class Calculator {
     add(input: string): number {
         if (input === "")
             return 0;
-        if (input.length === 1)
-            return parseInt(input);
         const numbers = input.split(",");
-        if (input.length > 2) {
-            return numbers.reduce((sum, number) => sum + parseInt(number), 0);
-        }
-        return parseInt(numbers[0]) + parseInt(numbers[1]);
+        return numbers.reduce((sum, number) => sum + parseInt(number), 0);
     }
 }
