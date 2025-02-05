a91300abbe24c585f06365301e342ce7d38deebb
ramvignesh-b <ramvignesh@incubyte.co>
2025-02-05 17:06:37 +0530

    feat: should return the sum of 2 numbers separated by a comma

--- src/calculator.ts
+++ src/calculator.ts
@@ -2,6 +2,9 @@ export class Calculator {
     add(input: string): number {
         if (input === "")
             return 0;
-        return parseInt(input);
+        if (input.length === 1)
+            return parseInt(input);
+        const numbers = input.split(",");
+        return parseInt(numbers[0]) + parseInt(numbers[1]);
     }
 }
