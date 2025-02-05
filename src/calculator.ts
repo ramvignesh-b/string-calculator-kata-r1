566432ec221c23e978d2455fcfd350e17de14625
ramvignesh-b <ramvignesh@incubyte.co>
2025-02-05 17:19:33 +0530

    refactor: apply SRP for parsing the numbers from the input

--- src/calculator.ts
+++ src/calculator.ts
@@ -2,7 +2,11 @@ export class Calculator {
     add(input: string): number {
         if (input === "")
             return 0;
-        const numbers = input.replace("\n", ",").split(",");
-        return numbers.reduce((sum, number) => sum + parseInt(number), 0);
+        const numbers = this.parseNumbers(input);
+        return numbers.reduce((sum, number) => sum + number, 0);
+    }
+
+    private parseNumbers(input: string): number[] {
+        return input.replace("\n", ",").split(",").map(Number);
     }
 }
